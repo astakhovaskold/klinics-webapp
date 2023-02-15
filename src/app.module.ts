bec0c0606e58313012182e8532ed113e0c1ded49
Askold Astakhov <astahov@element-soft.com>
2023-02-15 16:37:31 +0300

    mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import {Module} from '@nestjs/common';
 
+import {MongooseModule} from '@nestjs/mongoose';
+
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        MongooseModule.forRoot(
+            `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@localhost:27017/${process.env.MONGO_DATABASE}`,
+        ),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
