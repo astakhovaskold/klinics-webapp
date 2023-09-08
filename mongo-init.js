d8a2de3bbb30ef862c8334ebc6466ed995d6c09c
Askold Astakhov <astakhovaskold@gmail.com>
2023-09-08 09:16:04 +0300

    update env

--- mongo-init.js
+++ mongo-init.js
@@ -2,7 +2,7 @@
  * Created by ASTAKHOV A.A. on 15.02.2023
  */
 
-db = new Mongo().getDB('chameleondb');
+db = new Mongo().getDB('medicindb');
 
 db.createUser({
     user: 'askold_astakhov',
@@ -10,7 +10,7 @@ db.createUser({
     roles: [
         {
             role: 'readWrite',
-            db: 'chameleondb',
+            db: 'medicindb',
         },
     ],
 });
@@ -19,7 +19,7 @@ db.createCollection('users', {capped: false});
 
 db.users.insertOne({
     username: 'admin',
-    password: '$2b$10$x6eE.nOHN0ULIjcDcZuD3OdQxnQSWAE.3rjsOoZ/IwbvZ7kbWF.Ie',
+    password: '$2y$10$n7YDQKIXusjRD5jY2O8dq.lzV0tsMoxl9oU/NfrQtGOfzfqOwg8TC',
     role: 'ADMIN',
     is_active: true,
 });
